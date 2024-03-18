315dbdc7759503ce7fa8ca060fa810a5e6e59fc4
SmritiDewangan31 <smritidewangan06@gmail.com>
2024-03-18 10:37:42 +0530

    Change

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+
+	public static void main(String[] args) {
+		String name=args[0];  //hard coded value
+		System.out.println("Hello "+name);
+		System.out.println(args[1]);
+	}
+
+}
